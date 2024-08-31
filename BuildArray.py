2a06afe70d75e8bdfd739fc41e5778c484d26fdc
RitesHTechBuzz <166979619+RitesHTechBuzz@users.noreply.github.com>
2024-08-31 12:25:05 +0530

    Build Array

---
+++ BuildArray.py
@@ -0,0 +1,8 @@
+# Create another array which returns the value from the original array :
+
+class Solution:
+    def buildArray(self,nums:List[int])->List[int]:
+        ans = []
+        for i in range (len(nums)):
+            ans = append.(nums(nums[i]))
+        return ans
