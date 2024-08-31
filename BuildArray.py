df5cd8f8e635fc1cc5f6263ba3afd66791952bde
RitesHTechBuzz <166979619+RitesHTechBuzz@users.noreply.github.com>
2024-08-31 12:26:03 +0530

    Fixed some errors

--- BuildArray.py
+++ BuildArray.py
@@ -4,5 +4,5 @@ class Solution:
     def buildArray(self,nums:List[int])->List[int]:
         ans = []
         for i in range (len(nums)):
-            ans = append.(nums(nums[i]))
+            ans.append(nums(nums[i]))
         return ans
