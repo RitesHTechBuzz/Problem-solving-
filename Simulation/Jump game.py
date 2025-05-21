dfe3d4b25c2a81c60015aeff91914ab572a76f62
RitesHTechBuzz <166979619+RitesHTechBuzz@users.noreply.github.com>
2025-05-21 19:54:20 +0530

    Jump game

---
+++ Simulation/Jump game.py
@@ -0,0 +1,9 @@
+#based on the integer in array skip till that number , return True if reached at last of array.
+
+def jumpgame(self,nums:list[int])->bool:
+    max_reach = 0
+    for i in range(len(nums)):
+        if i> max_reach:
+            return False
+        max_reach  = max(max_reach,i+nums[i])
+    return True 
