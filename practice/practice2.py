4be1c095878f398cd0c17d8fb42f5b81e94229b3
RitesHTechBuzz <166979619+RitesHTechBuzz@users.noreply.github.com>
2025-07-16 00:46:46 +0530

    revision

--- practice/practice2.py
+++ practice/practice2.py
@@ -1,4 +1,10 @@
 # or via list and map functions where we it is not required to input the size of the list already given 
-inputs = input("Enter the elements separated by space:")
-List_elements = list(map(int,inputs.split()))
-print(List_elements)
+#nputs = input("Enter the elements separated by space:")
+#List_elements = list(map(int,inputs.split()))
+#print(List_elements)
+#x = memoryview(bytes(5))
+#print(x)#
+
+mylist = ['apple', 'banana', 'cherry']
+mylist[0] = 'kiwi'
+print(mylist[1])
